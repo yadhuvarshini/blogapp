7742d82863ed178ec6ef8fc04a9a2d08cee74009
yadhuvarshini <yadhuvarshini1@gmail.com>
2022-11-03 13:48:02 +0530

    demo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,10 @@
 import React,{useState,useEffect} from 'react';
-import {getDocs , collection,deleteDoc,doc} from 'firebase/firestore';
+import {getDocs , collection} from 'firebase/firestore';
 import {auth,db} from '../firebase-config';
 
- const Home = (isAuth) => {
+ const Home = ( {isAuth} ) => {
   const [postList , setPostList] = useState([]);
   const postsCollection = collection(db,"posts");
-
-  const deletePost = async (id) => {
-    const postDoc = doc(db,"posts",id)
-    await deleteDoc(postDoc);
-
-};
 
   useEffect(() =>{
     const getPosts = async () => {
@@ -19,7 +13,7 @@ import {auth,db} from '../firebase-config';
     };
 
     getPosts();
-  },[deletePost]);
+  });
 
   
 
@@ -32,18 +26,7 @@ import {auth,db} from '../firebase-config';
               <div className="title">
                 <h1> {post.title}</h1>
               </div>
-              <div className="deletePost">
-              {isAuth && post.author.id === auth.currentUser.uid && (
-                  <button
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  >
-                    {" "}
-                    &#128465;
-                  </button>
-                )}
-              </div>
+              
             </div>
             <div className="postTextContainer"> {post.postText} </div>
             {/* <h3>@{post.author.name}</h3> */}
